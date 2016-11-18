a2abf8fd3c4845d4b8c64926c459f37b3fd20df5
RyanLafferty <rlafferty1@hotmail.com>
2016-11-18 16:39:02 -0500

    updated sql setup file

--- api/db/tables.sql
+++ api/db/tables.sql
@@ -1,12 +1,39 @@
 CREATE DATABASE IF NOT EXISTS TheSeed;
 USE TheSeed;
 
-CREATE TABLE IF NOT EXISTS users
+CREATE TABLE IF NOT EXISTS Users
 (
     id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
     fname varchar(255),
     lname varchar(255),
     email varchar(255),
-    pass varchar(255),
+    pass varchar(255)
+);
 
+CREATE TABLE IF NOT EXISTS Retailers
+(
+    id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name varchar(255),
+    url varchar(2047)
+);
+
+CREATE TABLE IF NOT EXISTS Products
+(
+    id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name varchar(255),
+    quantity int,
+    dateCreated date,
+    dateScraped date,
+    price double precision,
+    source varchar(2047)
+);
+
+CREATE TABLE IF NOT EXISTS GardenFreshBoxes
+(
+    id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+);
+
+CREATE TABLE IF NOT EXISTS PodOrderForms
+(
+    id int AUTO_INCREMENT NOT NULL PRIMARY KEY,
 );
--- api/index.py
+++ api/index.py
@@ -1,10 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
+#index
 @app.route('/')
 def index():
     return 'index'
 
-#main
 if __name__ == '__main__':
     app.run(debug=True)
