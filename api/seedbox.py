3e58091a2db941ffb1d6a1f092c96cd4d63c3b05
Matthew Westrik <m@ttwestrik.com>
2016-11-18 16:44:01 -0500

    Add flask-sqlalchemy + restless

--- api/seedbox.py
+++ api/seedbox.py
@@ -1,22 +1,67 @@
-import os
-from flask import Flask, \
-		  send_from_directory
-#from flask_restful import Resource, Api
+import os, logging
+from logging import FileHandler
 
+from flask import Flask
+import flask_sqlalchemy
+import flask_restless
+
+
+# Set up application
+# ==========================================================================================
 application = Flask(__name__)
-#api = Api(application)
 
-@application.route("/api/")
-def hello():
-    return "<h1 style='color:blue'>SEEDBOX API</h1>"
+application.config['SQLALCHEMY_TRACK_NOTIFICATIONS'] = True
+application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/TheSeedSA'
+
+db = flask_sqlalchemy.SQLAlchemy(application)
 
 
-#class HelloWorld(Resource):
-#    def get(self):
-#        return {'hello': 'world'}
-#
-#api.add_resource(HelloWorld, '/')
+# Set up models
+# ==========================================================================================
+# Create your Flask-SQLALchemy models as usual but with the following two
+# (reasonable) restrictions:
+#   1. They must have a primary key column of type sqlalchemy.Integer or
+#      type sqlalchemy.Unicode.
+#   2. They must have an __init__ method which accepts keyword arguments for
+#      all columns (the constructor in flask.ext.sqlalchemy.SQLAlchemy.Model
+#      supplies such a method, so you don't need to declare a new one).
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode(128), unique=True)
+    birth_date = db.Column(db.Date)
 
+
+class Computer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Unicode(128), unique=True)
+    vendor = db.Column(db.Unicode(128))
+    purchase_time = db.Column(db.DateTime)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    owner = db.relationship('User', backref=db.backref('computers',
+                                                         lazy='dynamic'))
+
+# Set up corresponding RESTful API
+# ==========================================================================================
+# Create the database tables.
+db.create_all()
+
+# Create the Flask-Restless API manager.
+manager = flask_restless.APIManager(application, flask_sqlalchemy_db=db)
+
+# Create API endpoints, which will be available at /api/<tablename> by
+# default. Allowed HTTP methods can be specified as well.
+manager.create_api(User, methods=['GET', 'POST', 'DELETE'])
+manager.create_api(Computer, methods=['GET'])
+
+
+# Misc. routes
+# ==========================================================================================
+#@application.route("/api/")
+#def hello():
+#    return "<h1 style='color:blue'>SEEDBOX API</h1>"
+
+
+# ==========================================================================================
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
 
--- api/seedboxenv/bin/activate.csh
+++ api/seedboxenv/bin/activate.csh
@@ -7,7 +7,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/ubuntu/seedbox/seedboxenv"
+setenv VIRTUAL_ENV "/home/ubuntu/TheSeedBox/api/seedboxenv"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
