00cf1a717856aea162e124ca646e0d3101fbb384
Ryan Lafferty <rlafferty1@hotmail.com>
2016-11-21 23:43:52 -0500

    messing with flask alch api

--- api/tests/search.py
+++ api/tests/search.py
@@ -4,9 +4,9 @@ import json
 url = 'https://seedbox.tk/api/Users'
 headers = {'Content-Type': 'application/json'}
 
-filters = [dict(fname='Test')]
+filters = [dict(name='fname', op='like', val='Test')]
 params = dict(q=json.dumps(dict(filters=filters)))
-print params
 
 response = requests.get(url, params=params, headers=headers)
+assert response.status_code == 200
 print(response.json())
