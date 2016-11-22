881898b69c118ffc8767ae3c9ef474c22728838b
Ryan Lafferty <rlafferty1@hotmail.com>
2016-11-21 19:43:06 -0500

    messing around with flask restful

--- api/seedbox.py
+++ api/seedbox.py
@@ -1,7 +1,7 @@
 import os, logging
 from logging import FileHandler
 
-from flask import Flask
+from flask import Flask, request, flash, url_for, redirect, render_template
 import flask_sqlalchemy
 import flask_restless
 #from flask_restful import reqparse, abort, Api, Resource
@@ -62,56 +62,13 @@ class PodOrderForms(db.Model):
     __tablename__ = 'PodOrderForms'
     id = db.Column('id', db.Integer, primary_key=True)
 
-"""
-parser = reqparse.RequestParser()
-parser.add_argument('id')
-
-class TestIns(Resource):
-    def put(self, id):
-        args = parser.parse_args()
-        num = {'id': args['id']}
-        TODOS[id] = num
-        return num, 201
-"""
-
-
-"""
-
-TODOS = {
-    'todo1': {'task': 'build an API'},
-    'todo2': {'task': '?????'},
-    'todo3': {'task': 'profit!'},
-}
-
-
-def abort_if_todo_doesnt_exist(todo_id):
-    if todo_id not in TODOS:
-        abort(404, message="Todo {} doesn't exist".format(todo_id))
-
-parser = reqparse.RequestParser()
-parser.add_argument('task')
-
-
-# Todo
-# shows a single todo item and lets you delete a todo item
-
-class Todo(Resource):
-    def get(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        return TODOS[todo_id]
-
-    def delete(self, todo_id):
-        abort_if_todo_doesnt_exist(todo_id)
-        del TODOS[todo_id]
-        return '', 204
-
-    def put(self, todo_id):
-        args = parser.parse_args()
-        task = {'task': args['task']}
-        TODOS[todo_id] = task
-        return task, 201
-
-        """
+@app.route('/Test', methods = ['POST'])
+def Test():
+   if request.method == 'POST':
+      if not request.form['fname']:
+         flash('Please enter all the fields', 'error')
+      else:
+         return redirect('https://seedbox.tk/api/Users')
 
 
 # Set up corresponding RESTful API
