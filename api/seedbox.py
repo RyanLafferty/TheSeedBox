47abc332ce963e7a6ba7108da28f44195331c5d3
RyanLafferty <rlafferty1@hotmail.com>
2016-11-23 13:52:56 -0500

    cleaing up code

--- api/seedbox.py
+++ api/seedbox.py
@@ -65,11 +65,6 @@ class GardenFreshBoxes(db.Model):
 class PodOrderForms(db.Model):
     __tablename__ = 'PodOrderForms'
     id = db.Column('id', db.Integer, primary_key=True)
-"""
-class abc():
-    def Test():
-        return jsonify(message="Address book created successfully")
-"""
 
 # Set up corresponding RESTful API
 # ==========================================================================================
@@ -86,7 +81,6 @@ manager.create_api(Retailers, methods=['GET', 'POST', 'DELETE'])
 manager.create_api(Products, methods=['GET', 'POST', 'DELETE'])
 manager.create_api(PodOrderForms, methods=['GET', 'POST', 'DELETE'])
 manager.create_api(GardenFreshBoxes, methods=['GET', 'POST', 'DELETE'])
-#manager.create_api(abc, methods=['GET'])
 
 # Misc. routes
 # ==========================================================================================
