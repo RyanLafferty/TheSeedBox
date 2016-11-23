af1b25956a5d42046d13c581005ce45e38f5289c
Ryan Lafferty <rlafferty1@hotmail.com>
2016-11-22 21:00:07 -0500

    messing with api

--- api/tests/search.py
+++ api/tests/search.py
@@ -1,13 +1,14 @@
 import requests
 import json
 
-url = 'https://seedbox.tk/api/Users'
+url = 'https://seedbox.tk/api/Retailers'
 headers = {'Content-Type': 'application/json'}
 
-filters = [dict(name='fname', op='like', val='Test')]
-params = dict(q=json.dumps(dict(filters=filters)))
+#filters = [dict(name='fname', op='like', val='Test')]
+#params = dict(q=json.dumps(dict(filters=filters)))
+params = (json.dumps(dict(id=3, name='Walmart', url='www.walmart.ca')))
 print params
 
-response = requests.get(url, params=params, headers=headers)
-assert response.status_code == 200
+response = requests.post(url, params=params, headers=headers)
+#assert response.status_code == 200
 print(response.json())
