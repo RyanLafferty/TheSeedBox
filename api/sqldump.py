8ee1cb486a1ce1c945f18cffe769bf43e1db2af3
RyanLafferty <rlafferty1@hotmail.com>
2016-11-24 21:39:31 -0500

    working on backup script

--- api/sqldump.py
+++ api/sqldump.py
@@ -23,9 +23,9 @@ backupDir = upload
 datetime = time.strftime('%m%d%Y-%H%M%S')
 datetimeBackupDir = backupDir + datetime
 
-print "creating backup folder"
-if not os.path.exists(datetimeBackupDir):
-    os.makedirs(datetimeBackupDir)
+#print "creating backup folder"
+#if not os.path.exists(datetimeBackupDir):
+#    os.makedirs(datetimeBackupDir)
 
 
 #mysqldump_cmd = "mysqldump -u " + db_User_Name + " --password='" + DB_User_Password + "' -h mysql.server --databases '" + DB_Name + "' > " + datetimeBackupDir + "/" + DB_Name + ".sql"
