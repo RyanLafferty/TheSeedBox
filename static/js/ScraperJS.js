1801c142cfb73c64bf3d9c7bfac6443e29c49b3e
slawomirgawron <slawomirgawron8@gmail.com>
2016-11-25 03:44:04 -0500

    early testing for scraper page

--- static/js/ScraperJS.js
+++ static/js/ScraperJS.js
@@ -6,6 +6,8 @@ $.get("menu_admin.html", function(data){
 });
 
 //--------------------------------------------------------------------------------------
+var id = 0;
+
 // {
 //   "num_results": 1,
 //   "objects": [
@@ -26,8 +28,6 @@ $.get("menu_admin.html", function(data){
 // change settings
 // FYI the day of week enum is: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] have monday be 0, etc
 function createJsonChangeSettings(data) {
-    console.log("begin");
-    console.log(data);
     var  storeNameList = [];
     var storeString = " ";
     for (var i = 0; i < data["num_results"]; i++ ) {
@@ -58,8 +58,9 @@ function createJsonChangeSettings(data) {
                 if ( data["objects"][i][key] == 1 ) {
                     storeNameList.push( "nofrills" );
                 }
+            } else if ( key == "ide" ) {
+                id = data["objects"][i][key];
             }
-
         }
 
         // add retialers to current savings
@@ -70,15 +71,72 @@ function createJsonChangeSettings(data) {
             }
         }
         $('#stores').val( storeString );
-
-            // newCell.innerHTML = data["objects"][i][key];
-
     }
 
 }
 
+function dayToIntDropdown() {
+    if ( $('#DayDropdown').val() == "Monday") {
+        return 0;
+    } else if ( $('#DayDropdown').val() == "Tuesday") {
+        return 1;
+    } else if ( $('#DayDropdown').val() == "Wednesday") {
+        return 2;
+    } else if ( $('#DayDropdown').val() == "Thursday") {
+        return 3;
+    } else if ( $('#DayDropdown').val() == "Friday") {
+        return 4;
+    } else if ( $('#DayDropdown').val() == "Saturday") {
+        return 5;
+    } else if ( $('#DayDropdown').val() == "Sunday") {
+        return 6;
+    } else {
+        return 11;
+    }
+}
 
+function isCheckedNoFrills() {
+    if ( $('#NoFrills').is(":checked") ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
 
+function isCheckedMetro() {
+    if ( $('#Metro').is(":checked") ) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+function changeScrapaerSettings() {
+
+    var settings = {
+        "dayofweek": dayToIntDropdown(),
+        "metro_enabled": isCheckedMetro(),
+        "nofrills_enabled": isCheckedNoFrills(),
+        "time": $('#time').val(),
+        "user_id": 0
+    }
+
+    return settings;
+}
+
+$('#SaveButton').click(function(){
+    callSettings();
+    $.ajax({
+        type: 'PUT',
+        contentType:"application/json",
+        url: '/api/ScraperSettings/' + id,
+        data: JSON.stringify(changeScrapaerSettings()),  // data passed to db
+        dataType: 'json',
+        success: function (getData) { // y is waht the get returns
+            alert("Settings Updated");
+        }
+    });
+});
 
 
 // populate the current settings
