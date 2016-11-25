bb91dc66bce81e3c35ea3c3ca75f1d09e0607fad
slawomirgawron <slawomirgawron8@gmail.com>
2016-11-25 01:50:40 -0500

    early testing

--- static/js/ScraperJS.js
+++ static/js/ScraperJS.js
@@ -29,7 +29,9 @@ function createJsonChangeSettings(data) {
 
     for (var i = 0; i < data["num_results"]; i++ ) {
         for ( var key in data.objects[i]) {
+            console.log(key);
             if ( key == "dayofweek" ) {
+                console.log("dropdown");
                 if ( data["objects"][i][key] == 0 ) {
                     $('#day').val( 'Mon' );
                 } else if ( data["objects"][i][key] == 1 ) {
