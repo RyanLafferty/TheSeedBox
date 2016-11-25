6c7d8ba4f93908272a72977c60dd4a4a229eb47e
cckeating <ckeating@mail.uoguelph.ca>
2016-11-25 03:22:18 -0500

    test2

--- static/js/GFBJS.js
+++ static/js/GFBJS.js
@@ -45,7 +45,7 @@ function addDataToGFB(data) {
                 }
                 newCell.appendChild(selector);
                 /*selector.addEventListener('change', updateTotal(selector), false);*/
-                selector.onclick = updateTotal(selector);
+                selector.onchange = updateTotal(selector);
             } else if (key == 'item') {
                 newCell.innerHTML = data["objects"][i][key];
             } else {
